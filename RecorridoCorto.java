ee18782d466eea4c4a41210f04a653767b4aa748
jess1252 <jesusgolden100@gmail.com>
2025-08-14 13:14:09 -0500

    Add files via upload

---
+++ RecorridoCorto.java
@@ -0,0 +1,11 @@
+public class RecorridoCorto {
+    public static void main(String[] args) {
+        // Arreglo con 5 elementos
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        // Recorrido corto usando for-each
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
+}
