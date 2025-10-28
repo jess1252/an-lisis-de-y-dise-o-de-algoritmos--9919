e56e0c87735e065cf937b06e42be4b6efc165c74
jess1252 <jesusgolden100@gmail.com>
2025-10-27 22:24:22 -0500

    Actualizar GenerarContrasenaGUI.java

--- GenerarContrasenaGUI.java
+++ GenerarContrasenaGUI.java
@@ -19,7 +19,7 @@ public class GenerarContrasenaGUI {
     static String[] tablaHash = new String[10];
 
     public static void main(String[] args) {
-        // Crear las ventanas
+        // Crear todas  las ventanas
         JFrame frame = new JFrame("Generador de Contraseñas - con búsqueda");
         frame.setSize(520, 380);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
